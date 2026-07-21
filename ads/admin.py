e1269ae119d0ce35bfd244d051914763d364e307
bilialtymyshov-web <bilialtymyshov@gmail.com>
2026-07-21 04:39:45 -0700

    kjffdkjhgb fhdjgbefjkd  fjnvdk

--- ads/admin.py
+++ ads/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from .models import Category, Ad, AdImage
 
-# Register your models here.
+
+class AdImageInline(admin.TabularInline):
+    model = AdImage
+    extra = 3
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'slug')
+    prepopulated_fields = {'slug': ('name',)}
+
+
+@admin.register(Ad)
+class AdAdmin(admin.ModelAdmin):
+    list_display = ('title', 'category', 'price', 'status', 'author', 'views_count', 'created_at')
+    list_filter = ('status', 'category', 'created_at')
+    search_fields = ('title', 'description')
+    inlines = [AdImageInline]
